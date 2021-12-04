722c3fa955d48747871a66eef4a2861c4ba7d856
jinfanv <infanpearl@gmail.com>
2021-12-03 19:26:18 -0500

    Init

---
+++ renderer/pages/_app.js
@@ -0,0 +1,9 @@
+import 'antd/dist/antd.css';
+import '../styles/global.css'
+import '../styles/login.css'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,4 +1,10 @@
-import { useState, useEffect } from 'react'
+/* import 'antd/dist/antd.css';
+import '../styles/global.css'
+import '../styles/login.css' */
+import { Layout, Menu, Breadcrumb, Row, Col ,Image, Space,Typography} from 'antd';
+import React, { useState ,useEffect} from 'react';
+import Login from '../components/login';
+const { Title } = Typography;
 
 const Home = () => {
   const [input, setInput] = useState('')
@@ -20,7 +26,20 @@ const Home = () => {
   }
 
   return (
-    <div>
+
+
+    <Layout style={{ minHeight: '100vh' }}>
+        <Row  style={{ marginTop: '200px' }} justify='center'  >
+          <Col span={12} offset={9}>         
+              <Space align="center" direction="vertical">
+                <Image alt="" src="https://cdn.booklick.net/public/favicon-96x96.png"/>
+                <Title>Administrador</Title>          
+              </Space>                     
+            <Login/>
+          </Col>
+        </Row>       
+     </Layout>
+   /*  <div>
       <h1>Hello Electron!</h1>
 
       {message && <p>{message}</p>}
@@ -39,7 +58,7 @@ const Home = () => {
           font-size: 50px;
         }
       `}</style>
-    </div>
+    </div> */
   )
 }
 
